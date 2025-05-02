c275a6cc1ed898824e26026c854b9a79609fc5ed
Giuliano Gonzales <92641300+giugonzales07@users.noreply.github.com>
2025-05-02 17:57:25 -0300

    feat: main function

--- src/generation.py
+++ src/generation.py
@@ -9,9 +9,19 @@ load_dotenv(override=True)
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
 
-def generate_response(prompt, texts_relevant, model):
-    prompt_complete = f"Responda o aluno que escreveu: {prompt} para te guiar em como responder siga as instruções a seguir: {texts_relevant}. Lembre que você é um professor de inglês e deve responder o aluno como se fosse um professor. Não adicione informações que não façam parte do texto. Responda de forma clara e objetiva."
+def generate_response(prompt, texts_relevant, chat):
+    context = "\n\n".join(texts_relevant) if isinstance(texts_relevant, list) else texts_relevant
 
-    response = model.generate_content(prompt_complete)
+    #prompt_complete = f"Responda o aluno que escreveu: {prompt} para te guiar em como responder siga as instruções a seguir: {context}. Lembre que você é um professor de inglês e deve responder o aluno como se fosse um professor. Não adicione informações que não façam parte do texto. Responda de forma clara e objetiva."
+
+    prompt_complete = (
+        f"Você é um professor de inglês. Um aluno escreveu a seguinte pergunta:\n"
+        f"\"{prompt}\"\n\n"
+        f"Use o conteúdo abaixo como base para responder:\n"
+        f"{context}\n\n"
+        f"Responda de forma clara, objetiva, e sem inventar informações que não estejam no texto acima."
+    )
+
+    response = chat.send_message(prompt_complete)
 
     return response
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from src.embeddings import process_and_embed
 from src.retrieval import find_text_relevant
+from src.generation import generate_response
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
@@ -10,49 +11,57 @@ def to_markdown(text):
   formatted_text = text.replace('*', '')
   return textwrap.indent(formatted_text, ' ', predicate=lambda _: True)
 
-# Load the API key from the .env file
-load_dotenv(override=True)
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=GOOGLE_API_KEY)
+def create_model(model_name):
+  # Load the API key from the .env file
+  load_dotenv(override=True)
+  GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+  genai.configure(api_key=GOOGLE_API_KEY)
 
-# Create a new instance of the model
-model_name = "gemini-1.5-pro-002"
-katron = genai.GenerativeModel(model_name=model_name)
+  # Create a new instance of the model
+  model = genai.GenerativeModel(model_name=model_name)
+  return model
 
-# Start a new chat
-chat = katron.start_chat(history=[])
-
-print("\nOlá! Eu sou o Katron, um assistente de estudos. Vamos começar?\n")
-print("Digite 'exit' para sair.\n")
-print(to_markdown("Katron: O que vamos estudar hoje?"))
-prompt = input(" User: ")
-
-prompt_initial = "Eu quero ter uma aula de: "
-
-while prompt != "exit":
-    #print(to_markdown(f'user: {prompt}') + '\n')
-
-    response = chat.send_message(prompt_initial + prompt)
-    
-    print(to_markdown(f'\nKatron: {response.candidates[0].content.parts[0].text}'))
-    print("-------------------------------------------\n")
-    prompt = input(" User: ")
-
+def create_chat(model):
+  # Start a new chat
+  chat = model.start_chat(history=[])
+  return chat
 
 def main():
-  model = "models/embedding-001"
+  model_gen = "gemini-1.5-pro"
+  model_emb = "models/embedding-001"
   input_json_file = "data/how_teach.json"
   output_json_file = "data/how_teach_embeddings.json"
-
+  
+  katron = create_model(model_gen)
+  chat = create_chat(katron)
 
   if not os.path.exists(output_json_file):
     print("Embeddings não encontrados. Gerando...")
-    process_and_embed(input_json_file, output_json_file, model)
+    process_and_embed(input_json_file, output_json_file, model_emb)
   else:
-    print("Embeddings já existem. Pulando geração.")
+    print("Embeddings já existem. Pulando geração.\n")
 
+  print("\nOlá! Eu sou o Katron, seu assistente de estudos de idiomas. Vamos começar?\n")
+  print("Digite 'exit' para sair.\n")
+  print(to_markdown("Katron: O que vamos estudar hoje?"))
+  prompt = input(" User: ")
 
-  texts_relevant = find_text_relevant(prompt, output_json_file, model)
+  prompt_initial = "Eu quero ter uma aula de: "
+  first = True
+
+  while prompt != "exit":
+      #print(to_markdown(f'user: {prompt}') + '\n')
+      if first:
+        response = chat.send_message(prompt_initial + prompt)
+        first = False
+      else:
+        texts_relevant = find_text_relevant(prompt, output_json_file, model_emb)
+        response = generate_response(prompt, texts_relevant, chat)
+
+      print(to_markdown(f'\nKatron: {response.candidates[0].content.parts[0].text}'))
+      print("-------------------------------------------\n")
+      prompt = input(" User: ")
+
 
 if __name__ == "__main__":
     main()
